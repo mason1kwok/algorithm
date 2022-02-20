1f3d23d3472e6f716ae051bd5f4bc356a8c67cda
Mason Kwok <mason1kwok@gmail.com>
2022-02-20 22:41:47 +0800

    98. Validate Binary Search Tree

---
+++ go/0098.ValidateBinarySearchTree/main.go
@@ -0,0 +1,22 @@
+package main
+
+func isValidBST(root *TreeNode) bool {
+	return helper(root, nil, nil)
+}
+
+func helper(root *TreeNode, min *TreeNode, max *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	// 如果当前节点的值小于最小值，不是一个有效的二叉搜索树
+	if min != nil && root.Val <= min.Val {
+		return false
+	}
+	// 如果当前节点的值大于最大值，不是一个有效的二叉搜索树
+	if max != nil && root.Val >= max.Val {
+		return false
+	}
+
+	// 返回当前节点的左右子树验证结果
+	return helper(root.Left, min, root) && helper(root.Right, root, max)
+}
