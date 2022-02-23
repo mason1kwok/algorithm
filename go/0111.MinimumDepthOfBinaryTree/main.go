1417946beed56ae5b5e641be223589cb66ec283c
Mason Kwok <mason1kwok@gmail.com>
2022-02-23 23:04:32 +0800

    111. Minimum Depth of Binary Tree

---
+++ go/0111.MinimumDepthOfBinaryTree/main.go
@@ -0,0 +1,31 @@
+package main
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	stack := []*TreeNode{}
+	level := 0
+	stack = append(stack, root)
+
+	for len(stack) > 0 {
+		level++
+		l := len(stack)
+		for i := 0; i < l; i++ {
+			node := stack[0]
+			stack = stack[1:]
+			if node.Left == nil && node.Right == nil {
+				return level
+			}
+			if node.Left != nil {
+				stack = append(stack, node.Left)
+			}
+			if node.Right != nil {
+				stack = append(stack, node.Right)
+			}
+		}
+	}
+
+	return level
+
+}
