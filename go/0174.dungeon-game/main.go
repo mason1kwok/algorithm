0fb57e6dc7cf662259fbe3ecb3468b2be4e5b86a
Mason Kwok <mason1kwok@gmail.com>
2022-03-02 21:56:57 +0800

    174. Dungeon Game

---
+++ go/0174.dungeon-game/main.go
@@ -0,0 +1,56 @@
+package main
+
+var memo [][]int
+
+func calculateMinimumHP(dungeon [][]int) int {
+	m := len(dungeon)
+	n := len(dungeon[0])
+	// 初始化备忘录
+	memo = make([][]int, m)
+	for i, _ := range memo {
+		memo[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			memo[i][j] = -1
+		}
+	}
+	// 从0,0开始走
+	return dp(dungeon, 0, 0)
+}
+
+func dp(grid [][]int, i, j int) int {
+	m := len(grid)
+	n := len(grid[0])
+
+	// base case
+	if i == m-1 && j == n-1 {
+		if grid[i][j] >= 0 {
+			return 1
+		}
+		return -grid[i][j] + 1
+	}
+
+	if i == m || j == n {
+		return math.MaxInt
+	}
+
+	if memo[i][j] != -1 {
+		return memo[i][j]
+	}
+
+	// 状态转移逻辑
+	res := min(dp(grid, i, j+1), dp(grid, i+1, j)) - grid[i][j]
+
+	memo[i][j] = res
+	if res <= 0 {
+		memo[i][j] = 1
+	}
+	// 返回结果
+	return memo[i][j]
+}
+
+func min(a, b int) int {
+	if b > a {
+		return a
+	}
+	return b
+}
