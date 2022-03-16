3782ca5d2038d52e46965f2e82518cc03e4593d0
Mason Kwok <mason1kwok@gmail.com>
2022-03-16 23:32:21 +0800

    113. Path Sum II

---
+++ go/0113.path-sum-ii/main.go
@@ -0,0 +1,35 @@
+package main
+
+var res [][]int
+
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	res = [][]int{}
+	if root == nil {
+		return res
+	}
+	dfs(root, targetSum, []int{})
+	return res
+}
+
+func dfs(root *TreeNode, sum int, path []int) {
+	if root == nil {
+		return
+	}
+	remain := sum - root.Val
+	if root.Left == nil && root.Right == nil {
+		if remain == 0 {
+			path = append(path, root.Val)
+			res = append(res, append([]int{}, path...))
+			path = path[:len(path)-1]
+		}
+		return
+	}
+
+	path = append(path, root.Val)
+	dfs(root.Left, remain, path)
+	path = path[:len(path)-1]
+
+	path = append(path, root.Val)
+	dfs(root.Right, remain, path)
+	path = path[:len(path)-1]
+}
