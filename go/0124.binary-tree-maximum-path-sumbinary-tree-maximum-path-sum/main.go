567ccc0b52d10b73d9685b10fd2f7a66e6d0dca5
Mason Kwok <mason1kwok@gmail.com>
2022-04-20 16:01:42 +0800

    124. Binary Tree Maximum Path Sum

---
+++ go/0124.binary-tree-maximum-path-sumbinary-tree-maximum-path-sum/main.go
@@ -0,0 +1,29 @@
+package main
+
+func maxPathSum(root *TreeNode) int {
+	sum := math.MinInt32
+
+	var maxGain func(*TreeNode) int
+	maxGain = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		leftGain := max(0, maxGain(node.Left))
+		rightGain := max(0, maxGain(node.Right))
+
+		pathMaxSum := node.Val + leftGain + rightGain
+
+		sum = max(sum, pathMaxSum)
+
+		return node.Val + max(leftGain, rightGain)
+	}
+	maxGain(root)
+	return sum
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
